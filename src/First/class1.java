f29d89601f606628cf3cb5d410210df0d537f1cb
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2022-12-09 16:31:35 +0000

    added new work

--- src/First/class1.java
+++ src/First/class1.java
@@ -3,5 +3,6 @@ package First;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("created git repository");
+        System.out.println("more commit");
     }
 }
